d6cf47a2dfa0c5960671e3dc963b39631215aa16
Alex <alexandr.pogrebenniy@gmail.com>
2015-11-03 20:15:29 +0200

    First commit

---
+++ CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+    private Quaternion rotate;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+        rotate = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+        transform.rotation = rotate;
+	}
+}
